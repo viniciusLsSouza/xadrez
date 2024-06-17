e0a3c475991e4aad9b10a31876aa63d9f02ac163
Vinicius lacerda <viniciusls.lacerda@gmail.com>
2024-06-17 08:22:58 -0300

    primeiro

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+import java.sql.SQLOutput;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+    }
+}
